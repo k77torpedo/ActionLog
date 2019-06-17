06ae2f811c88687aef1d779fa1d1263da958857c
DEV-Segaia <segaia@gmx.de>
2019-06-17 19:37:16 +0200

    + Added Documentation image + Added String-RingBuffers and ActionLogs

---
+++ RingBuffers/RingBufferString.cs
@@ -0,0 +1,72 @@
+﻿namespace ActionLog {
+    /// <summary>
+    /// A forward 'FILO'-Ringbuffer storing <see cref="string"/> as data. The index '0' always is the last item added.
+    /// </summary>
+    public struct RingBufferString {
+        //Fields
+        private string[]             _buffer;
+        private int                 _capacity;
+        private int                 _count;
+        private int                 _head;
+
+        public int                  Capacity                { get { return _capacity; } }
+        public int                  Count                   { get { return _count; } }
+        public int                  Head                    { get { return _head; } }
+        public bool                 IsInitialized           { get { return _buffer != null; } }
+
+        public string this[int i] { 
+            get {
+                if (_head - i < 0) {
+                    return _buffer[_head - i + _capacity];
+                } else {
+                    return _buffer[_head - i];
+                }
+            }
+        }
+
+
+        //Functions
+        /// <summary>
+        /// Initializes the buffer according to capacity.
+        /// </summary>
+        /// <param name="pCapacity">The capacity of the buffer.</param>
+        public void Init (int pCapacity) {
+            _capacity = pCapacity;
+            _buffer = new string[pCapacity];
+        }
+
+        /// <summary>
+        /// Adds the data to the next slot in the buffer.
+        /// </summary>
+        /// <param name="pData">The data.</param>
+        public void Add (string pData) {
+            _head = (_head + 1) % _capacity;
+            _buffer[_head] = pData;
+            if (_count < _capacity) {
+                _count++;
+            }
+        }
+
+        /// <summary>
+        /// Translates an external index used to access elements in the buffer to the respective internal index.
+        /// </summary>
+        /// <param name="pIndex">The index to translate.</param>
+        /// <returns>The internal index of the buffer.</returns>
+        public int TranslateIndex (int pIndex) {
+            if (_head - pIndex < 0) {
+                return _head - pIndex + _capacity;
+            } else {
+                return _head - pIndex;
+            }
+        }
+
+        /// <summary>
+        /// Clears the buffer.
+        /// </summary>
+        public void Clear () {
+            _head = _capacity - 1;
+            _buffer[_head] = null;
+            _count = 0;
+        }
+    }
+}
